e6fe3a59b562cad37c8561f944958131dfd79700
GCL <lewin.gc@gmail.com>
2017-12-26 20:30:23 -0500

    obsolete

--- weather_copter.cpp
+++ weather_copter.cpp
@@ -8,23 +8,99 @@
 
 #include <weather_copter.h>
 
-bool WeatherCopter::CheckForNewDatums(void)
+
+uint8_t WeatherCopter::CheckForGPSDatum(void)
 {
     bool retVal = 0;
     
-    if(gps->CheckSerial() & GGA)
+    GPSdatum datum;
+    if(gps.CheckSerial(&datum) == GGA)
     {
-        //currGPSstring = gps->GetReading().MakeShortDataString();
-//            WriteSD(filename, gps->GetReading().MakeShortDataString());
-    }
-
-    if(anemometer->CheckSerial())
-    {
-        //ReadTemperature();
-        //ReadPressure();
-//            WriteSD(filename, trisonica->GetReading().MakeDataString() + String(',') + currGPSstring);
+        gpsDatum = datum;
+        retVal = gpsDatum.source;
     }
     
     return retVal;
 }
 
+bool WeatherCopter::CheckForWindDatum(void)
+{
+    bool retVal = false;
+    
+    TrisonicaDatum newDatum;
+    if(anemometer.CheckSerial(&newDatum))
+    {
+        windDatum = newDatum;
+        retVal = true;
+    }
+    
+    return retVal;
+}
+
+bool WeatherCopter::CheckForIMUUpdate(void)
+{
+    bool retVal = false;
+    
+    AHRSDatum newDatum;
+    if(imu.CheckInput(&newDatum))
+    {
+        ahrsDatum = newDatum;
+        retVal = true;
+    }
+    
+    return retVal;
+}
+
+bool WeatherCopter::CheckForAlt280Update(void)
+{
+    bool retVal = false;
+    
+    AltimeterDatum newDatum;
+    if(altimeter280.CheckForNewDatum(newDatum))
+    {
+        altDatum280 = newDatum;
+        retVal = true;
+    }
+    
+    return retVal;
+}
+
+bool WeatherCopter::CheckForAlt3115Update(void)
+{
+    bool retVal = false;
+    
+    AltimeterDatum newDatum;
+    if(altimeter3115.CheckForNewDatum(newDatum))
+    {
+        altDatum3115 = newDatum;
+        retVal = true;
+    }
+    
+    return retVal;
+}
+
+uint8_t WeatherCopter::CheckSensors(void)
+{
+    uint8_t retVal = 0;
+    if(CheckForGPSDatum() == GGA) //only need GGA for the weather copter
+    {
+        retVal |= GGA;
+    }
+
+    if(CheckForWindDatum())
+    {
+        retVal |= TRISONICA;
+    }
+
+    if(CheckForAlt280Update())
+    {
+        retVal |= ALT280;
+    }
+
+    if(CheckForIMUUpdate())
+    {
+        retVal |= IMU;
+    }
+    
+    return retVal;
+}
--- weather_copter.h
+++ weather_copter.h
@@ -6,41 +6,77 @@
 //
 //
 
-#ifndef weather_copter_h
-#define weather_copter_h
+//#ifndef weather_copter_h
+//#define weather_copter_h
+
+#pragma once
 
 #include <trisonica.h>
 #include <gps.h>
 #include <MPL3115A2.h>
+#include <BME280.h>
+#include <LSM9DS1.h>
 
-struct WeatherDatum
-{
-    TrisonicaDatum windDatum;
-    GPSdatum gpsDatum;
-    AltimeterDatum altDatum;
-
-    //TempPressDatum
-    //OtherDatums?
-};
+#define TRISONICA 0x10
+#define ALT280 0x20
+#define IMU 0x40
 
 class WeatherCopter
 {
 protected:
-    GPS* gps = NULL;
-    Trisonica* anemometer = NULL;
-    Altimeter* altimeter = NULL;
+    TrisonicaDatum windDatum;
+    GPSdatum gpsDatum;
+    AltimeterDatum altDatum3115;
+    AltimeterDatum altDatum280;
+    AHRSDatum ahrsDatum;
+
+    Trisonica anemometer;
+    GPS_MTK3339 gps;
+    BME280 altimeter280;
+    MPL3115A2 altimeter3115;
+    LSM9DS1 imu;
     
-    WeatherDatum workingDatum;
 public:
-    WeatherCopter(void) {}
+    WeatherCopter(HardwareSerial* triSer, HardwareSerial* gpsSer) : anemometer(triSer), gps(gpsSer)
+    {}
     
-    bool CheckForNewDatums(void);
+    bool Init(void)
+    {
+        altimeter280.Init();
+        imu.Init();
+        gps.Init(); //doesn't really init; just sends data rate stuff
+
+        imu.CalibrateMagnetometer(100);
+
+        return 1;
+    }
     
+    uint8_t CheckForGPSDatum(void);
+    bool CheckForWindDatum(void);
+    bool CheckForIMUUpdate(void);
+    bool CheckForAlt280Update(void);
+    bool CheckForAlt3115Update(void);
+    
+    uint8_t CheckSensors(void);
+    
+    bool SetActiveNMEAStrings(uint8_t nmea) {return gps.SetActiveNMEAStrings(nmea);}
+    int MakeFilename(void)
+    {
+        return gpsDatum.MakeMonthDay();
+    }
+
     String MakeDataString(void)
     {
-        String datum = workingDatum.gpsDatum.MakeShortDataString() + workingDatum.windDatum.MakeDataString();
+        String datum = gpsDatum.MakeShortDataString() + windDatum.MakeDataString()
+                       + altDatum280.MakeDataString() + ahrsDatum.MakeDataString();
+        return datum;
+    }
+
+    String MakeShortDataString(void)
+    {
+        String datum = gpsDatum.MakeShortDataString() + windDatum.MakeDataString();
         return datum;
     }
 };
 
-#endif /* weather_copter_h */
+//#endif /* weather_copter_h */
